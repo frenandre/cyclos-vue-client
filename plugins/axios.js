fe8e1fc73547eea1e1d23d087f2d05f04dd3f7f3
Andreas Frena <andreas.frena@yandex.com>
2018-05-04 11:57:34 +0200

    update

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,6 +1,5 @@
 export default function ({ $axios, store }) {
   $axios.onRequest(config => {
-    console.log('Making request to ' + config.url)
     if (store.state.auth) {
       config.headers.common['Authorization'] = 'Basic ' + store.state.auth
     }
